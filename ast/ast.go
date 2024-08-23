c8cfd8aa690b340a5d169521de99a92e76be5b77
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-08-22 20:45:13 -0700

    Add infix parsing

--- ast/ast.go
+++ ast/ast.go
@@ -145,3 +145,25 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString(")")
 	return out.String()
 }
+
+// Infix Expressions
+type InfixExpression struct {
+	Token    token.Token
+	Operator string
+	Left     Expression
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.TokenLiteral() }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
