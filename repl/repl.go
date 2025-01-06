3727614a770a00b77532d1adc64554a4666a565c
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2025-01-05 17:18:27 -0800

    Update repl to parse input using lexer
    - Update repl.go to parse input program
    - Add repl parse errors pretty printing

--- repl/repl.go
+++ repl/repl.go
@@ -6,8 +6,21 @@ import (
 	"io"
 
 	"github.com/KSSidhu/go-interpreter/lexer"
-	"github.com/KSSidhu/go-interpreter/token"
+	"github.com/KSSidhu/go-interpreter/parser"
 )
+
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 const PROMPT = ">> "
 
@@ -26,10 +39,24 @@ func Start(in io.Reader, out io.Writer) {
 		// Pass input to lexer to parse
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		// Iterate over tokens from input and print to console
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Whoops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
